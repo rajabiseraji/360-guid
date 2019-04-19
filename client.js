2593ed95f8f5d02dbbbd44c8d9655169cfa7f53e
rajabiseraji <rajabiseraji@aut.ac.ir>
2019-04-19 05:03:44 +0430

    feat(multisurface): adds four cylinders

--- client.js
+++ client.js
@@ -1,7 +1,7 @@
 // This file contains the boilerplate to execute your React app.
 // If you want to modify your application's content, start in "index.js"
 
-import {ReactInstance} from 'react-360-web';
+import {ReactInstance, Surface} from 'react-360-web';
 
 function init(bundle, parent, options = {}) {
   const r360 = new ReactInstance(bundle, parent, {
@@ -10,10 +10,31 @@ function init(bundle, parent, options = {}) {
     ...options,
   });
 
-  // Render your app content to the default cylinder surface
+  const forwardPanel = new Surface(1000, 600, Surface.SurfaceShape.Flat);
+  forwardPanel.setAngle(0, 0);
+  const rightPanel = new Surface(1000, 600, Surface.SurfaceShape.Flat);
+  rightPanel.setAngle(Math.PI / 2, 0);
+  const leftPanel = new Surface(1000, 600, Surface.SurfaceShape.Flat);
+  leftPanel.setAngle(Math.PI, 0);
+  const rearPanel = new Surface(1000, 600, Surface.SurfaceShape.Flat);
+  rearPanel.setAngle(-Math.PI / 2, 0);
+
+
   r360.renderToSurface(
-    r360.createRoot('Hello360', { /* initial props */ }),
-    r360.getDefaultSurface()
+    r360.createRoot('RightPanel'),
+    rightPanel,
+  );
+  r360.renderToSurface(
+    r360.createRoot('ForwardPanel'),
+    forwardPanel,
+  );
+  r360.renderToSurface(
+    r360.createRoot('RearPanel'),
+    rearPanel,
+  );
+  r360.renderToSurface(
+    r360.createRoot('LeftPanel'),
+    leftPanel,
   );
 
   // Load the initial environment
