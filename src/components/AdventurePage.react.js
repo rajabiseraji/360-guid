faf334dbc29a6eab049f86f14d0d95922a9e2555
rajabiseraji <rajabiseraji@aut.ac.ir>
2019-04-21 19:42:44 +0430

    feat(adventure page): adds it

---
+++ src/components/AdventurePage.react.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import {
+  Environment,
+  StyleSheet,
+  View,
+  asset
+} from 'react-360';
+
+// import MainPageButton from 'MainPageButton.react';
+
+export default class AdventurePage extends React.Component {
+  constructor(props) {
+    super(props);
+    Environment.setBackgroundImage(asset('adventurebg.jpg'));
+    this.state = {
+      index: 0
+    };
+  }
+
+  _onClick = (index) => {
+    this.setState({
+      index: index
+    });
+  };
+
+  render() {
+    return null
+  }
+};
+
+// defining StyleSheet
+// const styles = StyleSheet.create({
+//   panel: {
+//     width: 400,
+//     height: 300,
+//     backgroundColor: 'red',
+//     justifyContent: 'center',
+//     alignItems: 'center',
+//     padding: 20,
+//     transform: [
+//       {translate: [0, 0, -4]}
+//     ]
+//   }
+// });
