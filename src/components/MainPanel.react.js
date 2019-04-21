b942c8179cc406321faf927c77e3f166621f186e
rajabiseraji <rajabiseraji@aut.ac.ir>
2019-04-22 03:50:04 +0430

    feat(mainPanel): adds main panel component

---
+++ src/components/MainPanel.react.js
@@ -0,0 +1,182 @@
+import React from 'react';
+import {
+    StyleSheet,
+    View,
+    asset,
+    Image
+} from 'react-360';
+
+import MiscButton from 'MiscButton.react';
+
+const questionIcon = asset('help.png');
+const homeIcon = asset('home.png');
+const backIcon = asset('back.png');
+
+export default class MainPage extends React.Component {
+    static defaultProps = {
+        isHomePage: false,  
+        images: []
+    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      showHelp: false
+    };
+  }
+  
+  _onClick = () => {
+    this.setState({
+      showHelp: !this.state.showHelp
+    });
+  };
+
+  _renderHelp = () => {
+      return (
+        <View style={
+            [
+                styles.mainPanel,
+                {
+                    justifyContent: 'flex-end',
+                    marginRight: 300,
+                    height: 500,
+                    transform: [{
+                        translate: [0, 0, -400]
+                    }]
+                }
+            ]
+        }>
+            <Image 
+                style = {styles.helpText}
+                source = {
+                    asset('mainHelp.png')
+                }
+            />  
+        </View>
+      )
+  }
+
+  _renderPageImages = () => {
+        return (
+            <View style = {styles.mainPanel}>
+                {
+                    this.props.images.map((image) => {
+                        (<MainPageButton
+                            style = {styles.button}
+                            source = {image.src}
+                            onClick = {
+                                () => {
+                                    this.props.history.push(image.routeUrl);    
+                                }
+                            } 
+                        />)
+                    })
+                }
+                {
+                    !this.props.isHomePage ? 
+                    <MiscButton
+                        style = {styles.backButton}
+                        source = {backIcon}
+                        onClick = {this.props.history.go(-1)}
+                    /> : null
+                }
+            </View>
+        );
+  }
+
+  render() {
+      return ( 
+        <View style = {
+            [
+                styles.panel,
+                {
+                    flexDirection: !this.state.showHelp ? 'column' : 'row'
+                }
+
+            ]
+        }>
+            {!this.state.showHelp ? this._renderPageImages() : this._renderHelp()}
+            <View style = {
+                styles.miscPanel
+            }>
+                {
+                    this.props.isHomePage ? 
+                    <Image
+                        style={styles.confused} 
+                        source = {
+                            asset('confused.png')
+                        }
+                    /> : null
+                }
+                <MiscButton
+                    style = {styles.miscButton}
+                    source = {questionIcon}
+                    onClick = {this._onClick}
+                />
+                <MiscButton
+                    style = {styles.miscButton}
+                    source = {homeIcon}
+                    onClick = {this.props.history.push('/')}
+                />
+            </View>
+        </View>
+      );
+    }
+  };
+
+// defining StyleSheet
+const styles = StyleSheet.create({
+  panel: {
+    flex: 1,
+    flexDirection: 'row',
+    width: 4096,
+    height: 600,
+    backgroundColor: 'transparent',
+    justifyContent: 'space-around',
+    alignItems: 'center',
+    padding: 20,
+    transform: [
+      {translate: [0, -100, -4]}
+    ]
+  },
+  mainPanel: {
+    flexDirection: 'row',
+    width: 4096,
+    height: 300,
+    backgroundColor: 'transparent',
+    justifyContent: 'space-around',
+    alignItems: 'center',
+    padding: 20,
+  },
+  miscPanel: {
+      flexDirection: 'row',
+      justifyContent: 'flex-end',
+      position: 'absolute',
+      right: 0,
+      left: 0,
+      bottom: 0,
+      height: 100
+  },
+  miscButton: {
+    marginLeft: 10,
+    marginRight: 10,
+  },
+  noTransform: {
+    transform: [
+      {translate: [0, 0, 0]}
+    ]
+  },
+  button: {
+      marginRight: 100,
+      marginLeft: 100
+  },
+  helpText: {
+    height: '100%',
+    aspectRatio: 1.66,
+  },
+  confused: {
+      height: '100%',
+      aspectRatio: 2.14,
+      marginLeft: 50,
+      marginRight: 10
+  }
+});
