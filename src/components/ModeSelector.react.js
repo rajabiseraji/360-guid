87e6d1b9355374d4b17626874eb8daa1b2b6a2b1
rajabiseraji <rajabiseraji@aut.ac.ir>
2019-04-22 05:55:12 +0430

    feat(modeSelector): inits mode selector

--- src/components/ModeSelector.react.js
+++ src/components/ModeSelector.react.js
@@ -1,5 +1,42 @@
 import React from 'react';
 
 export default class ModeSelector extends React.Component {
-    
+    static defaultProps = {
+        onChangeMode: () => {},
+        weather: 'sunny',
+        timeOfDay: 'day'
+    }
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            weather: 'sunny',
+            timeOfDay: 'day'
+        }
+    }
+
+    _onWeatherChange(newWeather) {
+        this.setState({
+            weather: newWeather
+        })
+    }
+
+    _onTimeOfDayChange(newTimeOfDay) {
+        this.setState({
+            timeOfDay: newTimeOfDay
+        })
+    }
+
+    _onApplySettings() {
+        this.props.onChangeMode({
+            weather: this.state.weather,
+            timeOfDay: this.state.timeOfDay
+        });
+    }
+
+    render() {
+        return (
+
+        )
+    }
 }
