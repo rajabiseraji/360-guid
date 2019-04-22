83c2d6ab6b4cc732243d21cde3f98134b2575a01
rajabiseraji <rajabiseraji@aut.ac.ir>
2019-04-22 05:49:12 +0430

    feat(placePage): adds mode selection to place page

--- src/components/PlacePage.react.js
+++ src/components/PlacePage.react.js
@@ -8,6 +8,19 @@ export default class PlacePage extends React.Component {
     super(props);
     const {name, category} = props.match.params;
     Environment.setBackgroundImage(asset(`/${category}/${name}/day.jpg`));
+    this.state = {
+      timeOfDay: 'day',
+      weather: 'sunny'
+    }
+  }
+
+  _changeMode(newTimeOfDay, newWeather) {
+    this.setState({
+      timeOfDay: newTimeOfDay, 
+      weather: newWeather
+    });
+    const newWeatherString = newWeather !== 'sunny' ? `_${newWeather}` : '';
+    Environment.setBackgroundImage(asset(`/${category}/${name}/${newTimeOfDay}${newWeatherString}.jpg`))
   }
   
   render() {
